855ef5975f45990e2c04c420c0fd261fd8de2ce7
Brennan Colberg <brennan.colberg@gmail.com>
2018-03-01 21:59:29 -0800

    More Advanced Camera
    added cameraInit(), copy-pasted in some code from
    https://wpilib.screenstepslive.com/s/currentCS/m/vision/l/669166-using-the-cameraserver-on-the-roborio

--- src/org/usfirst/frc/team3268/robot/Robot.java
+++ src/org/usfirst/frc/team3268/robot/Robot.java
@@ -7,12 +7,17 @@
 
 package org.usfirst.frc.team3268.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team3268.robot.commands.ExampleCommand;
 import org.usfirst.frc.team3268.robot.subsystems.ButterflyWingsSubsystem;
 import org.usfirst.frc.team3268.robot.subsystems.DriveSubsystem;
 import org.usfirst.frc.team3268.robot.subsystems.ShooterPneumaticsSubsystem;
 import org.usfirst.frc.team3268.robot.subsystems.ShooterWheelsSubsystem;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -47,7 +52,7 @@ public class Robot extends TimedRobot {
 	 */
 	public void robotInit() {
 		
-		CameraServer.getInstance().startAutomaticCapture();
+		cameraInit();
 		
 		oi = new OI();
 		
@@ -56,6 +61,24 @@ public class Robot extends TimedRobot {
 		m_chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", m_chooser);
+	}
+	public void cameraInit() {
+		new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            camera.setResolution(640, 480);
+            
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+            
+            Mat source = new Mat();
+            Mat output = new Mat();
+            
+            while(!Thread.interrupted()) {
+                cvSink.grabFrame(source);
+                Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                outputStream.putFrame(output);
+            }
+        }).start();
 	}
 
 	/**
